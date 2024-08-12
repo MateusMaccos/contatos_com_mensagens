1be2f827d6c57d2ddff79065b075d5f86fb0b41b
Mateus Machado Costa <75508372+MateusMaccos@users.noreply.github.com>
2024-08-12 18:18:06 -0300

    adicionar lista de contatos

--- interface.py
+++ interface.py
@@ -5,7 +5,7 @@ import Pyro4
 import Pyro4.naming
 import threading
 import server
-
+import paho.mqtt.client as mqtt
 
 class ServidorNomes:
     def iniciar_servidor_nomes(self, ip):
@@ -17,11 +17,35 @@ class ServidorNomes:
         except:
             messagebox.showwarning("Aviso", "Erro ao criar servidor de nomes")
 
+class Mensagem:
+    def __init__(self,origem,destino,texto):
+        self.origem = origem
+        self.destino = destino
+        self.texto = texto
+
+
+class Usuario:
+    def __init__(self,nome):
+        self.user = mqtt.Client(
+            mqtt.CallbackAPIVersion.VERSION1,
+        )
+        self.user.connect(host="test.mosquitto.org", port=1883)
+        self.user.loop_start()
+
+        self.nome = nome
+        self.contatos = []
+        self.mensagens=[]
+    def getNome(self):
+        return self.nome
+    def enviarMensagem(self,origem,destino,texto):
+        msg = Mensagem(origem,destino,texto)
+        self.mensagens.append(msg)
+    def addContato(self,nome):
+        self.contatos.append(nome)
+    def removerContato(self,nome):
+        self.contatos.remove(nome)
 
 class Aplicacao:
-    def __init__(self):
-        self.usuarios = []
-
     def mudarTema(self):
         if self.tema == "dark":
             self.tema = "light"
@@ -72,26 +96,18 @@ class Aplicacao:
         t_sv.start()
 
     def tela_SV_iniciado(self):
-        self.erro = False
         ip_sn = self.entrada_IP_SN.get()
         ip_sv = self.entrada_ip_sv.get()
         nome_sv = self.entrada_nome_sv.get()
         try:
             Pyro4.locateNS(host=ip_sn, port=9090)
-        except Exception as e:
-            self.erro = True
-            messagebox.showerror("Erro", "Erro em localizar o servidor de nomes")
-        if not self.erro:
-
             self.iniciar_servidor(nome_sv, ip_sn, ip_sv)
             self.frame_SV.destroy()
 
-            self.frame_SV_iniciado = tk.Frame()
+            self.frame_SV_iniciado = tk.Frame(self.tela)
             self.frame_SV_iniciado.pack()
 
-            self.lbl_texto = tk.Label(
-                self.frame_SV_iniciado, text="Servidor de mensagens iniciado"
-            )
+            self.lbl_texto = tk.Label(self.frame_SV_iniciado, text="Servidor de mensagens iniciado")
             self.lbl_texto.pack(pady=10)
 
             self.lbl_ip_sv = tk.Label(self.frame_SV_iniciado, text=f"IP: {ip_sv}")
@@ -99,6 +115,22 @@ class Aplicacao:
 
             self.lbl_nome_sv = tk.Label(self.frame_SV_iniciado, text=f"Nome: {nome_sv}")
             self.lbl_nome_sv.pack(pady=10)
+
+            # self.lbl_usuarios = tk.Label(self.frame_SV_iniciado, text="Usuários cadastrados:")
+            # self.lbl_usuarios.pack(side=tk.TOP)
+
+            # frame_caixa_usuarios = tk.Frame(self.frame_SV_iniciado)
+            # frame_caixa_usuarios.pack(fill=tk.BOTH, expand=True)
+
+            # self.lb_usuarios = tk.Listbox(frame_caixa_usuarios, width=50, height=10)
+            # self.lb_usuarios.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+            # # Adiciona a barra de rolagem se necessário
+            # scrollbar = tk.Scrollbar(frame_caixa_usuarios, orient=tk.VERTICAL, command=self.lb_usuarios.yview)
+            # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+            # self.lb_usuarios.config(yscrollcommand=scrollbar.set)
+        except Exception as e:
+            messagebox.showerror("Erro", f"Erro ao localizar o servidor de nomes: {e}")
 
     def tela_SN_iniciar(self):
         self.tela_inicial_frame.destroy()
@@ -135,6 +167,7 @@ class Aplicacao:
         self.lbl_texto.pack(pady=10)
 
     def tela_agenda(self):
+        self.tela_inicial_frame.destroy()
         self.frame_agenda = tk.Frame()
         self.frame_agenda.pack()
 
@@ -152,6 +185,14 @@ class Aplicacao:
         self.entrada_ip_sn = ttk.Entry(self.frame_agenda)
         self.entrada_ip_sn.pack(pady=5)
 
+        self.lbl_nome_usuario = tk.Label(
+            self.frame_agenda, text="Nome de usuario"
+        )
+        self.lbl_nome_usuario.pack(pady=5)
+
+        self.entrada_nome_usuario = ttk.Entry(self.frame_agenda)
+        self.entrada_nome_usuario.pack(pady=5)
+
         self.botao_iniciar = ttk.Button(
             self.frame_agenda,
             text="Iniciar Agenda",
@@ -163,11 +204,47 @@ class Aplicacao:
     def tela_agenda_iniciado(self):
         nome_sv = self.entrada_nome_sv.get()
         ip_sn = self.entrada_ip_sn.get()
+        nome_usuario = self.entrada_nome_usuario.get()
+        usuario = Usuario(nome_usuario)
 
         self.frame_agenda.destroy()
 
         self.sv_mensagens = Pyro4.Proxy("PYRONAME:" + nome_sv + "@" + ip_sn + ":9090")
-        self.sv_mensagens.testarSM()
+
+        self.sv_mensagens.addUsuario(nome_usuario)
+
+        self.frame_agenda_iniciada = tk.Frame()
+        self.frame_agenda_iniciada.pack()
+
+        self.lbl_texto = tk.Label(
+            self.frame_agenda_iniciada, text="Contatos"
+        )
+        self.lbl_texto.pack(pady=10)
+        
+        self.entrada_nome_contato = ttk.Entry(self.frame_agenda_iniciada)
+        self.entrada_nome_contato.pack(pady=5)
+
+        self.botao_iniciar = ttk.Button(
+            self.frame_agenda_iniciada,
+            text="Adicionar contato",
+            style="Accent.TButton",
+            command= lambda: self.adicionarContato(usuario),
+        )
+        self.botao_iniciar.pack(pady=10)
+
+        self.lbl_usuarios = tk.Label(self.frame_agenda_iniciada, text="Contatos")
+        self.lbl_usuarios.pack(side=tk.TOP)
+
+        frame_caixa_usuarios = tk.Frame(self.frame_agenda_iniciada)
+        frame_caixa_usuarios.pack(fill=tk.BOTH, expand=True)
+
+        self.lb_usuarios = tk.Listbox(frame_caixa_usuarios, width=50, height=10)
+        self.lb_usuarios.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        # Adiciona a barra de rolagem se necessário
+        scrollbar = tk.Scrollbar(frame_caixa_usuarios, orient=tk.VERTICAL, command=self.lb_usuarios.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.lb_usuarios.config(yscrollcommand=scrollbar.set)
 
     def tela_inicial(self):
         self.tela_inicial_frame = tk.Frame(self.tela)
@@ -196,7 +273,10 @@ class Aplicacao:
             command=self.tela_agenda,
         )
         self.botao_agenda.pack(pady=10)
-
+    def adicionarContato(self,usuario):
+        contato = self.entrada_nome_contato.get()
+        usuario.addContato(contato)
+        self.lb_usuarios.insert(0,contato)
     def run(self):
         self.tela = tk.Tk()
         self.tela.title("Agenda")
--- server.py
+++ server.py
@@ -1,12 +1,40 @@
 import Pyro4
 
+class Mensagem:
+    def __init__(self,origem,destino,texto):
+        self.origem = origem
+        self.destino = destino
+        self.texto = texto
+
+class Usuario:
+    def __init__(self,nome):
+        self.nome = nome
+        self.contatos = []
+        self.mensagens=[]
+    def getNome(self):
+        return self.nome
+    def enviarMensagem(self,origem,destino,texto):
+        msg = Mensagem(origem,destino,texto)
+        self.mensagens.append(msg)
+    def addContato(self,nome):
+        self.contatos.append(nome)
+    def removerContato(self,nome):
+        self.contatos.remove(nome)
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
 class ServidorDeMensagens(object):
-    def testarSM(self):
-        print("TESTE")
-
+    def __init__(self):
+        self.usuarios = []
+    def addUsuario(self,user):
+        usuarioNovo = Usuario(user)
+        self.usuarios.append(usuarioNovo)
+        print(usuarioNovo.getNome())
+    def getTodosUsuarios(self):
+        nomes=[]
+        for usuario in self.usuarios:
+            nomes.append(usuario.nome)
+        return nomes
 
 def iniciar(nomeSV, ipNS, ipSV):
     daemon = Pyro4.Daemon(host=ipSV)
